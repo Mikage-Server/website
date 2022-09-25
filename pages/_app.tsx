c1d8751390762de9cf3274c594119ae8cdafa5ac
takara2314 <takara2314_TK@outlook.jp>
2022-09-25 17:45:23 +0900

    Add: `google-site-verification` を設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,7 @@ const MyApp = ({ Component, pageProps, router }: AppProps) => {
       <Head>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="icon" href="/favicon.webp" />
+        <meta name="google-site-verification" content="9MFGKY2INTNuKmR8Lq0XJCIbI2_iowGRlOGMZ-YGR-s" />
       </Head>
 
       <DefaultSeo
