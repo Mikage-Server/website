eafd024aa89c3ca805222f1ed1c61c347930497a
takara2314 <takara2314_TK@outlook.jp>
2022-09-26 10:54:25 +0900

    Change: scriptタグの場所を変更

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,10 @@ const MyApp = ({ Component, pageProps, router }: AppProps) => {
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="icon" href="/favicon.webp" />
         <meta name="google-site-verification" content="9MFGKY2INTNuKmR8Lq0XJCIbI2_iowGRlOGMZ-YGR-s" />
+        <script defer
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token": "124496809bcf4a82a14d220e48612dea"}'
+        />
       </Head>
 
       <DefaultSeo
@@ -49,13 +53,6 @@ const MyApp = ({ Component, pageProps, router }: AppProps) => {
       </AnimatePresence>
 
       <Menu />
-
-      {/* Cloudflare Web Analytics */}
-      <script defer
-        src="https://static.cloudflareinsights.com/beacon.min.js"
-        data-cf-beacon='{"token": "124496809bcf4a82a14d220e48612dea"}'
-      />
-      {/* End Cloudflare Web Analytics */}
     </>
   );
 };
