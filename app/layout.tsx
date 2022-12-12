9123af9924863c1450f16e88259ef2a3487e3564
takara2314 <takara2314_TK@outlook.jp>
2022-12-12 10:19:07 +0900

    Remove: Cloudflare Insights の手動設定を削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Script from 'next/script';
 import { AnimatePresence } from 'framer-motion';
 import useModern from '../hooks/useModern';
 import Menu from '../lib/menu/menu';
@@ -15,10 +14,6 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
       <head>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="icon" href="/favicon.webp" />
-        <Script defer
-          src="https://static.cloudflareinsights.com/beacon.min.js"
-          data-cf-beacon='{"token": "124496809bcf4a82a14d220e48612dea"}'
-        />
       </head>
 
       <body>
