0bac8a9f262b9c0e8ca743a28d6279dd3323e87d
takara2314 <takara2314_TK@outlook.jp>
2022-12-12 10:16:34 +0900

    Remove: 不要なverificationを削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,6 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
       <head>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="icon" href="/favicon.webp" />
-        <meta name="google-site-verification" content="9MFGKY2INTNuKmR8Lq0XJCIbI2_iowGRlOGMZ-YGR-s" />
         <Script defer
           src="https://static.cloudflareinsights.com/beacon.min.js"
           data-cf-beacon='{"token": "124496809bcf4a82a14d220e48612dea"}'
