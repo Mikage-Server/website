6e3a757f155cc9f68417b744a71deb7d2c94c297
takara2314 <takara2314_TK@outlook.jp>
2022-12-20 06:51:53 +0900

    Add: sample page

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import Main from '../components/main';
+
+const NotFound = () => {
+  return (
+    <Main>
+      <h1>あなたは未知のアクセスを試みた</h1>
+      このページは存在しません。
+    </Main>
+  );
+};
+
+export default NotFound;
---
+++ pages/sample.tsx
@@ -0,0 +1,12 @@
+import Main from '../components/main';
+
+const Sample = () => {
+  return (
+    <Main>
+      <h1>This is the sample page.</h1>
+      takara2314
+    </Main>
+  );
+};
+
+export default Sample;
