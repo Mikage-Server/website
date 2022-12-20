b8ef18b50b057ee2cbd54864215b786861e65f37
takara2314 <takara2314_TK@outlook.jp>
2022-12-20 09:20:01 +0900

    Improve: 404ページを改良

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
       <head>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="icon" href="/favicon.webp" />
+        <link rel="canonical" href={process.env.NEXT_PUBLIC_SITE_URL} />
       </head>
 
       <body>
--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,34 @@
-import Main from '../components/main';
+import Link from 'next/link';
+import '../app/globals.css';
 
 const NotFound = () => {
   return (
-    <Main>
-      <h1>あなたは未知のアクセスを試みた</h1>
-      このページは存在しません。
-    </Main>
+    <div className="w-screen h-screen bg-stripe-not-found">
+      <main className="text-white text-not-found w-screen h-screen bg-[rgba(255,0,0,0.5)] flex flex-col justify-center items-center">
+        <div className="mb-16 text-center">
+          <h1 className="text-3xl font-bold">
+            あなたは未知のアクセスを試みた
+          </h1>
+          <div className="mt-5 text-xl font-medium">
+            このページは存在しません。
+          </div>
+        </div>
+
+        <Link href="/">
+          <div className="border-4 border-black">
+            <button className="px-10 py-5 text-2xl font-medium bg-gray-400 border-4 border-t-gray-300 border-l-gray-300 border-r-gray-600 border-b-gray-600">
+              トップページに移動する
+            </button>
+          </div>
+        </Link>
+
+        <small className="mt-10 text-xl font-medium">
+          <a href="https://twitter.com/siojinja">
+            © 2022 御景サーバー
+          </a>
+        </small>
+      </main>
+    </div>
   );
 };
 
