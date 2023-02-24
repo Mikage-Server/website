d6466eb6b77ce82bd946775e5b5a53719d1d9d4e
takara2314 <takara2314_TK@outlook.jp>
2023-02-25 08:00:20 +0900

    Improve: ビルドしてからデプロイするように

---
+++ .github/createEnviron.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+// 環境変数をコマンドラインから取得
+const vars = require('minimist')(process.argv.slice(2));
+delete vars._;
+
+// 環境変数ファイルに書き込む内容を作成
+let content = 'env_variables:\n';
+Object.keys(vars).forEach((key) => {
+  content += `  ${key}: ${vars[key]}\n`;
+});
+
+// 環境変数ファイルを作成
+fs.writeFile('./environ.yaml', content, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('The file is created successfully.');
+  }
+});
