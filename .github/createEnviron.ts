86fd335a5204d53089bf0742c29ff624d838d8fb
takara2314 <takara2314_TK@outlook.jp>
2023-02-25 06:41:03 +0900

    Change: deploy on Google App Engine from Vercel

---
+++ .github/createEnviron.ts
@@ -0,0 +1,16 @@
+import { parse } from 'https://deno.land/std@0.178.0/flags/mod.ts';
+
+const vars = parse(Deno.args);
+
+let content = 'env_variables:\n';
+Object.keys(vars).forEach((key) => {
+  content += `  ${key}: ${vars[key]}\n`;
+});
+
+Deno.writeTextFile('./environ.yaml', content)
+  .then(() => {
+    console.log('The file is created successfully.');
+  })
+  .catch((err) => {
+    console.log(err);
+  })
