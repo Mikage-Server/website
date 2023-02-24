7355ce2c8887e0eba9b89ab63a33a3d0a322dc26
takara2314 <takara2314_TK@outlook.jp>
2023-02-25 06:53:24 +0900

    Improve: 不要なパラメータを環境変数ファイルに入れないように修正

--- .github/createEnviron.ts
+++ .github/createEnviron.ts
@@ -2,6 +2,8 @@ import { parse } from 'https://deno.land/std@0.178.0/flags/mod.ts';
 
 const vars = parse(Deno.args);
 
+delete vars._;
+
 let content = 'env_variables:\n';
 Object.keys(vars).forEach((key) => {
   content += `  ${key}: ${vars[key]}\n`;
