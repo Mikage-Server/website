4095bfff9090516ee90c45f408a9e46d9dbbcee6
takara2314 <takara2314_TK@outlook.jp>
2023-02-25 20:15:14 +0900

    Add: scroll down animation

--- app/page.tsx
+++ app/page.tsx
@@ -6,21 +6,29 @@ import Slide from './home/slide';
 import Version from './home/version';
 import Welcome from './home/welcome';
 import WhatsNew from './home/whatsNew';
+import ScrollDownAnimation from './home/scrollDownAnimation';
 
 const Home = () => {
   return (
     <Main>
-      <Slide />
+      <div className="relative">
+        <Slide />
+        <ScrollDownAnimation />
+      </div>
+
       <Welcome />
       <Intro />
       <WhatsNew />
+
       <div className="mt-20 w-full md:grid md:grid-cols-12 md:gap-6">
         <CrossPlay />
         <Version />
       </div>
+
       <LetsJoin />
     </Main>
   );
 };
+
 
 export default Home;
