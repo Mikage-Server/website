a5e9dd535989876f21e1a5fd758028ac7b8b0db7
takara2314 <takara2314_TK@outlook.jp>
2023-03-10 11:12:34 +0900

    Feat: フッターにリンクを貼るように

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,11 +1,25 @@
 const Footer = () => {
   return (
     <footer className="mt-16 w-full h-32 text-white bg-yellow-700 flex flex-col items-center justify-center">
-      <small className="text-base text-center">
-        <a href="https://twitter.com/siojinja">
+      <div>
+        <small className="text-base text-center">
           © 2022 御景サーバー
+        </small>
+      </div>
+
+      <div className="mt-1">
+        <a href={process.env.NEXT_PUBLIC_TWITTER_URL} className="px-2">
+          Twitter
         </a>
-      </small>
+        /
+        <a href={process.env.NEXT_PUBLIC_JMS_VOTE_URL} className="px-2">
+          Japan Minecraft Servers
+        </a>
+        /
+        <a href={process.env.NEXT_PUBLIC_MONOCRAFT_VOTE_URL} className="px-2">
+          monocraft
+        </a>
+      </div>
     </footer>
   );
 };
