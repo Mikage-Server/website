46dadc97146a584d7d67f33b62e73dcb5b4706e8
takara2314 <takara2314_TK@outlook.jp>
2023-05-05 18:51:41 +0900

    Remove: no-cache を削除してデータ取得を行うように

--- app/fetchStatus.ts
+++ app/fetchStatus.ts
@@ -23,8 +23,7 @@ export interface PlayerNum {
 
 const fetchStatus: () => Promise<Status> = async () => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/status`,
-    { cache: 'no-store' }
+    `${process.env.NEXT_PUBLIC_API_URL}/status`
   );
   return res.json();
 };
