9e678fe0643e45dbf4e860bf0ea84ee2c57aeaff
takara2314 <takara2314_TK@outlook.jp>
2024-03-27 11:54:41 +0900

    Remove: グローバルな状態管理

--- app/clientLayout.tsx
+++ app/clientLayout.tsx
@@ -1,15 +1,17 @@
 'use client';
 
+import { AnimatePresence } from 'framer-motion';
 import { usePathname } from 'next/navigation';
 import { useState } from 'react';
-import { Provider } from 'jotai';
-import { AnimatePresence } from 'framer-motion';
 import useColorTheme from '../hooks/useColorTheme';
-import Menu from './menu/menu';
 import './globals.css';
+import Menu from './menu/menu';
 
-const ClientLayout = ({ children }: { children: React.ReactNode }) => {
-  const [submenuName, setSubmenuName] = useState<string>('');
+const ClientLayout = ({
+  children,
+}: { children: React.ReactNode }) => {
+  const [submenuName, setSubmenuName] =
+    useState<string>('');
   const pathname = usePathname();
 
   // ユーザーのカラーテーマの適応をサポート
@@ -18,7 +20,7 @@ const ClientLayout = ({ children }: { children: React.ReactNode }) => {
   // 通常時はメニューを表示
   if (pathname !== '/status') {
     return (
-      <Provider>
+      <>
         <AnimatePresence
           initial={false}
           onExitComplete={() => window.scrollTo(0, 0)}
@@ -30,16 +32,12 @@ const ClientLayout = ({ children }: { children: React.ReactNode }) => {
           submenuName={submenuName}
           setSubmenuName={setSubmenuName}
         />
-      </Provider>
+      </>
     );
   }
 
   // ステータスページではメニューを非表示
-  return (
-    <Provider>
-      {children}
-    </Provider>
-  );
+  return children;
 };
 
 export default ClientLayout;
--- app/main.tsx
+++ app/main.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import { useAtom } from 'jotai';
 import Footer from './footer';
 
 const Main = ({
